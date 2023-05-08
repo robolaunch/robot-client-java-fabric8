ad54f3333f85fd70796de45e2df39448f09badfe
tunahanertekin <tunahanertekin@users.noreply.github.com>
2023-05-08 14:25:07 +0000

    build(ci): generate fabric8 models 0.1.0-0.2.5p-0.1.6p-0.1.7p-alpha.4 for robot.roboscale.io v0.2.5-alpha.9, fleet.roboscale.io v0.1.6-alpha.9 and connection-hub.roboscale.io v0.1.7-alpha.2

--- src/main/java/io/roboscale/robot/v1alpha1/robotspec/LaunchManagerTemplates.java
+++ src/main/java/io/roboscale/robot/v1alpha1/robotspec/LaunchManagerTemplates.java
@@ -1,41 +1,25 @@
 package io.roboscale.robot.v1alpha1.robotspec;
 
 @com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
-@com.fasterxml.jackson.annotation.JsonPropertyOrder({"display","launch"})
+@com.fasterxml.jackson.annotation.JsonPropertyOrder({"launches"})
 @com.fasterxml.jackson.databind.annotation.JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
 @javax.annotation.processing.Generated("io.fabric8.java.generator.CRGeneratorRunner")
 public class LaunchManagerTemplates implements io.fabric8.kubernetes.api.model.KubernetesResource {
 
     /**
-     * Launch processes connects an X11 socket if it's set to `true` and a target RobotVDI resource is set in labels with key `robolaunch.io/target-vdi`. Applications that requires GUI can be executed such as rViz.
-     */
-    @com.fasterxml.jackson.annotation.JsonProperty("display")
-    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Launch processes connects an X11 socket if it's set to `true` and a target RobotVDI resource is set in labels with key `robolaunch.io/target-vdi`. Applications that requires GUI can be executed such as rViz.")
-    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
-    private Boolean display;
-
-    public Boolean getDisplay() {
-        return display;
-    }
-
-    public void setDisplay(Boolean display) {
-        this.display = display;
-    }
-
-    /**
      * Launch descriptions. Every object defined here generates a launching command in the specified workspace.
      */
-    @com.fasterxml.jackson.annotation.JsonProperty("launch")
+    @com.fasterxml.jackson.annotation.JsonProperty("launches")
     @com.fasterxml.jackson.annotation.JsonPropertyDescription("Launch descriptions. Every object defined here generates a launching command in the specified workspace.")
     @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
-    private java.util.Map<java.lang.String, io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.Launch> launch;
+    private java.util.Map<java.lang.String, io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.Launches> launches;
 
-    public java.util.Map<java.lang.String, io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.Launch> getLaunch() {
-        return launch;
+    public java.util.Map<java.lang.String, io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.Launches> getLaunches() {
+        return launches;
     }
 
-    public void setLaunch(java.util.Map<java.lang.String, io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.Launch> launch) {
-        this.launch = launch;
+    public void setLaunches(java.util.Map<java.lang.String, io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.Launches> launches) {
+        this.launches = launches;
     }
 }
 
---
+++ src/main/java/io/roboscale/robot/v1alpha1/robotspec/launchmanagertemplates/launches/Container.java
@@ -0,0 +1,73 @@
+package io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.launches;
+
+@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+@com.fasterxml.jackson.annotation.JsonPropertyOrder({"display","env","privileged","resources"})
+@com.fasterxml.jackson.databind.annotation.JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
+@javax.annotation.processing.Generated("io.fabric8.java.generator.CRGeneratorRunner")
+public class Container implements io.fabric8.kubernetes.api.model.KubernetesResource {
+
+    /**
+     * Launch processes connects an X11 socket if it's set to `true` and a target RobotVDI resource is set in labels with key `robolaunch.io/target-vdi`. Applications that requires GUI can be executed such as rViz.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("display")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Launch processes connects an X11 socket if it's set to `true` and a target RobotVDI resource is set in labels with key `robolaunch.io/target-vdi`. Applications that requires GUI can be executed such as rViz.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private Boolean display;
+
+    public Boolean getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(Boolean display) {
+        this.display = display;
+    }
+
+    /**
+     * Additional environment variables to set when launching ROS nodes.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("env")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Additional environment variables to set when launching ROS nodes.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private java.util.List<io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.launches.container.Env> env;
+
+    public java.util.List<io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.launches.container.Env> getEnv() {
+        return env;
+    }
+
+    public void setEnv(java.util.List<io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.launches.container.Env> env) {
+        this.env = env;
+    }
+
+    /**
+     * Launch container privilege.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("privileged")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Launch container privilege.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private Boolean privileged;
+
+    public Boolean getPrivileged() {
+        return privileged;
+    }
+
+    public void setPrivileged(Boolean privileged) {
+        this.privileged = privileged;
+    }
+
+    /**
+     * Launch container resource limits.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("resources")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Launch container resource limits.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.launches.container.Resources resources;
+
+    public io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.launches.container.Resources getResources() {
+        return resources;
+    }
+
+    public void setResources(io.roboscale.robot.v1alpha1.robotspec.launchmanagertemplates.launches.container.Resources resources) {
+        this.resources = resources;
+    }
+}
+
