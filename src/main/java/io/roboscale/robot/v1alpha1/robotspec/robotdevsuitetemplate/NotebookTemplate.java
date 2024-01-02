bf3af1577abb9b62a12937aa881aa1a38821f38e
tunahanertekin <tunahanertekin@users.noreply.github.com>
2024-01-02 14:22:19 +0000

    build(ci): generate fabric8 models 0.1.0-alpha.18 for robot.roboscale.io v0.2.6-alpha.14.9, fleet.roboscale.io v0.1.6-alpha.14 and connection-hub.roboscale.io v0.1.7-alpha.10

---
+++ src/main/java/io/roboscale/robot/v1alpha1/robotspec/robotdevsuitetemplate/NotebookTemplate.java
@@ -0,0 +1,107 @@
+package io.roboscale.robot.v1alpha1.robotspec.robotdevsuitetemplate;
+
+@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+@com.fasterxml.jackson.annotation.JsonPropertyOrder({"display","ingress","privileged","resources","serviceType"})
+@com.fasterxml.jackson.databind.annotation.JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
+@javax.annotation.processing.Generated("io.fabric8.java.generator.CRGeneratorRunner")
+public class NotebookTemplate implements io.fabric8.kubernetes.api.model.KubernetesResource {
+
+    /**
+     * Notebook connects an X11 socket if it's set to `true` and a target Notebook resource is set in labels with key `robolaunch.io/target-vdi`. Applications that requires GUI can be executed such as VLC.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("display")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Notebook connects an X11 socket if it's set to `true` and a target Notebook resource is set in labels with key `robolaunch.io/target-vdi`. Applications that requires GUI can be executed such as VLC.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private Boolean display;
+
+    public Boolean getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(Boolean display) {
+        this.display = display;
+    }
+
+    /**
+     * [*alpha*] Notebook will create an Ingress resource if `true`.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("ingress")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("[*alpha*] Notebook will create an Ingress resource if `true`.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private Boolean ingress;
+
+    public Boolean getIngress() {
+        return ingress;
+    }
+
+    public void setIngress(Boolean ingress) {
+        this.ingress = ingress;
+    }
+
+    /**
+     * If `true`, containers of Notebook will be privileged containers. It can be used in physical instances where it's necessary to access I/O devices on the host machine. Not recommended to activate this field on cloud instances.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("privileged")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("If `true`, containers of Notebook will be privileged containers. It can be used in physical instances where it's necessary to access I/O devices on the host machine. Not recommended to activate this field on cloud instances.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private Boolean privileged;
+
+    public Boolean getPrivileged() {
+        return privileged;
+    }
+
+    public void setPrivileged(Boolean privileged) {
+        this.privileged = privileged;
+    }
+
+    /**
+     * Resource limitations of Notebook.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("resources")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Resource limitations of Notebook.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private io.roboscale.robot.v1alpha1.robotspec.robotdevsuitetemplate.notebooktemplate.Resources resources;
+
+    public io.roboscale.robot.v1alpha1.robotspec.robotdevsuitetemplate.notebooktemplate.Resources getResources() {
+        return resources;
+    }
+
+    public void setResources(io.roboscale.robot.v1alpha1.robotspec.robotdevsuitetemplate.notebooktemplate.Resources resources) {
+        this.resources = resources;
+    }
+
+    public enum ServiceType {
+
+        @com.fasterxml.jackson.annotation.JsonProperty("ClusterIP")
+        CLUSTERIP("ClusterIP"), @com.fasterxml.jackson.annotation.JsonProperty("NodePort")
+        NODEPORT("NodePort");
+
+        java.lang.String value;
+
+        ServiceType(java.lang.String value) {
+            this.value = value;
+        }
+
+        @com.fasterxml.jackson.annotation.JsonValue()
+        public java.lang.String getValue() {
+            return value;
+        }
+    }
+
+    /**
+     * Service type of Notebook. `ClusterIP` and `NodePort` is supported.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("serviceType")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Service type of Notebook. `ClusterIP` and `NodePort` is supported.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private ServiceType serviceType = io.fabric8.kubernetes.client.utils.Serialization.unmarshal("\"NodePort\"", ServiceType.class);
+
+    public ServiceType getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(ServiceType serviceType) {
+        this.serviceType = serviceType;
+    }
+}
+
