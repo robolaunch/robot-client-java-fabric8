354868eff235aba84aa0ff242ea6874adf04b84c
tunahanertekin <tunahanertekin@users.noreply.github.com>
2024-03-21 07:23:59 +0000

    build(ci): generate fabric8 models 0.1.0-alpha.31 for robot.roboscale.io v0.2.7-alpha.6.4, fleet.roboscale.io v0.1.6-alpha.18 and connection-hub.roboscale.io v0.1.8-alpha.4

---
+++ src/main/java/io/kubefed/types/v1beta1/FederatedCodeEditor.java
@@ -0,0 +1,10 @@
+package io.kubefed.types.v1beta1;
+
+@io.fabric8.kubernetes.model.annotation.Version(value = "v1beta1" , storage = true , served = true)
+@io.fabric8.kubernetes.model.annotation.Group("types.kubefed.io")
+@io.fabric8.kubernetes.model.annotation.Singular("federatedcodeeditor")
+@io.fabric8.kubernetes.model.annotation.Plural("federatedcodeeditors")
+@javax.annotation.processing.Generated("io.fabric8.java.generator.CRGeneratorRunner")
+public class FederatedCodeEditor extends io.fabric8.kubernetes.client.CustomResource<io.kubefed.types.v1beta1.FederatedCodeEditorSpec, io.kubefed.types.v1beta1.FederatedCodeEditorStatus> implements io.fabric8.kubernetes.api.model.Namespaced {
+}
+
